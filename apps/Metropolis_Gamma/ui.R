2d68c621d9abddff0df0cb3cb26f95fe4ebea552
Cristian Villegas <master.villegas@gmail.com>
2018-05-19 14:28:55 -0300

    Change wrong ui file

--- apps/Metropolis_Gamma/ui.R
+++ apps/Metropolis_Gamma/ui.R
@@ -1,11 +1,15 @@
 library("shiny")
  
-fluidPage(title="MH (independence)",
+fluidPage(title="Metropolis (dependence)",
 
     sidebarPanel(
       withMathJax(),
-      h3(strong("Sampling from Beta distribution (target) 
-                based on the proposal gamma distribution",style="color:black")),
+      h3(strong("Sampling from  Gamma distribution (target)
+                with shape and rate parameter
+                based on the proposal normal distribution
+                with tuning parameter (sigma)",style="color:black")),
+      br(),
+      br(),
       h3(strong("Behaviour of the MCMC",style="color:black")),
       h3(sliderInput("Iterations",
                   "Iterations",
@@ -19,51 +23,52 @@ fluidPage(title="MH (independence)",
                   label="Thin", 
                   value=1, min = 1, max = 1000,
                   step=10,animate=TRUE)),
-      h3(strong("Initial values",style="color:black")),
+      br(),
+      br(),
+      h3(strong("Initial Values",style="color:black")),
       h3(numericInput(inputId="theta.inicial.1", 
                    label=HTML("&theta;[1]"), 
-                   value=0.1, min = -5, max = 5,
+                   value=1, min = -5, max = 5,
                    step=0.5)),
-      h3(numericInput(inputId="theta.inicial.2",
+      h3(numericInput(inputId="theta.inicial.2", 
                    label=HTML("&theta;[2]"), 
-                   value=0.5, min = -5, max = 5,
+                   value=5, min = -5, max = 5,
                    step=0.5)),
-      h3(numericInput(inputId="theta.inicial.3",
+      h3(numericInput(inputId="theta.inicial.3", 
                    label=HTML("&theta;[3]"), 
-                   value=0.1, min = -5, max = 5,
+                   value=12, min = -5, max = 5,
                    step=0.5)),
-      h3(strong("Proposal Gamma distribution",style="color:black")),
-      h3(numericInput(inputId="shape",
+      h3(numericInput(inputId="tuning.parameter", 
+                   label=HTML("&sigma;"), 
+                   value=0.5, min = 0.5, max = 10,
+                   step=0.5)),
+      br(),
+      br(),
+      h3(strong("Target Gamma distribution",style="color:black")),
+      h3(numericInput(inputId="shape", 
                    label="Shape parameter", 
-                   value=0.3, min = 0.1, max = 4,
-                   step=0.1)),
+                   value=2, min = 0.1, max = 10,
+                   step=0.5)),
       h3(numericInput(inputId="rate", 
                    label="Rate parameter", 
-                   value=0.03, min = 0.1, max = 4,
-                   step=0.1)),
-      h3(strong("Target Beta distribution",style="color:black")),
-      h3(numericInput(inputId="shape1",
-                   label="Shape1 parameter", 
-                   value=2, min = 0.5, max = 10,
+                   value=5, min = 0.1, max = 10,
                    step=0.5)),
-      h3(numericInput(inputId="shape2", 
-                   label="Shape2 parameter", 
-                   value=5, min = 0.5, max = 10,
-                   step=0.5)),
+      br(),
+      br(),
       h3(strong("Reference")),
       h3(code("Siddhartha Chib & Edward Greenberg (2012). 
-               Understanding the Metropolis-Hastings 
-               Algorithm, The American Statistician, 49:4, 
-               327-335."))
+              Understanding the Metropolis-Hastings 
+              Algorithm, The American Statistician, 49:4, 
+              327-335."))
       
         ),
 
     mainPanel(
-       h1(strong("Metropolis Hasting algorithm (independence sampling)"), align="center"),
-       plotOutput("plot1"),
-       plotOutput("plot2"),
-       h3(strong("Summary of Chains",style="color:black")),
-       verbatimTextOutput("summary1")
-       )
-    
+      h1(strong("Metropolis algorithm (dependence sampling)"), align="center"),
+      plotOutput("plot1"),
+      plotOutput("plot2"),
+      h3(strong("Summary of Chains",style="color:black")),
+      verbatimTextOutput("summary1")
+          
+      )
     )
